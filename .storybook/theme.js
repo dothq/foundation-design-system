5110ea0bf0fac9affdb78d390aeda413e7b9fdb3
Jamie Thalacker <hello@jamiethalacker.dev>
2021-12-11 17:57:42 -0800

    :lipstick: Dot HQ Logo

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Dot UI',
+  brandUrl: 'https://dothq.co',
+  brandImage: 'https://staging.dothq.co/static/icons/57x57.png',
+});
