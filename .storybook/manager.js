37b1ea80284fb0b4f8e261d58948fd4ff3a78a6e
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-12-18 22:45:18 +0000

    💄 Update theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,8 @@
 import { addons } from '@storybook/addons';
 import theme from './theme'
 
+console.log("i")
+
 addons.setConfig({
   theme: theme,
   sidebar: {
--- .storybook/theme.js
+++ .storybook/theme.js
@@ -5,6 +5,17 @@ export default create({
   brandTitle: 'Dot UI',
   brandUrl: 'https://dothq.co',
   brandImage: 'https://staging.dothq.co/static/icons/57x57.png',
-
-  colorSecondary: 'rgb(22,98,211)'
+  appBg: 'white',
+  appContentBg: 'white',
+  appBorderColor: 'rgba(0, 0, 0, 0.1)',
+  appBorderRadius: 8,
+  colorSecondary: 'rgb(22, 98, 211)',
+  fontBase: '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+  textColor: 'black',
+  barTextColor: 'silver',
+  barBg: 'white',
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 2
 });
